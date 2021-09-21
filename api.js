951b7b51c495c4e5c086976482169620b97e7ce2
แทน <89076276+prataennaimjoi@users.noreply.github.com>
2021-09-21 20:47:48 +0700

    Create api.js

---
+++ api.js
@@ -0,0 +1,433 @@
+{
+  "type": "carousel",
+  "contents": [
+    {
+      "type": "bubble",
+      "body": {
+        "type": "box",
+        "layout": "horizontal",
+        "contents": [
+          {
+            "type": "text",
+            "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            "wrap": true
+          }
+        ]
+      },
+      "footer": {
+        "type": "box",
+        "layout": "horizontal",
+        "contents": [
+          {
+            "type": "button",
+            "style": "primary",
+            "action": {
+              "type": "uri",
+              "label": "Go",
+              "uri": "https://example.com"
+            }
+          }
+        ]
+      }
+    },
+    {
+      "type": "bubble",
+      "body": {
+        "type": "box",
+        "layout": "horizontal",
+        "contents": [
+          {
+            "type": "text",
+            "text": "Hello, World!",
+            "wrap": true
+          }
+        ]
+      },
+      "footer": {
+        "type": "box",
+        "layout": "horizontal",
+        "contents": [
+          {
+            "type": "button",
+            "style": "primary",
+            "action": {
+              "type": "uri",
+              "label": "Go",
+              "uri": "https://example.com"
+            }
+          }
+        ]
+      }
+    }
+  ]
+}
+{
+  "type": "bubble",
+  "styles": {
+    "header": {
+      "backgroundColor": "#ffaaaa"
+    },
+    "body": {
+      "backgroundColor": "#aaffaa"
+    },
+    "footer": {
+      "backgroundColor": "#aaaaff"
+    }
+  },
+  "header": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "header"
+      }
+    ]
+  },
+  "hero": {
+    "type": "image",
+    "url": "https://example.com/flex/images/image.jpg",
+    "size": "full",
+    "aspectRatio": "2:1"
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "body"
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "footer"
+      }
+    ]
+  }
+}
+{
+  "type": "bubble",
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "spacing": "md",
+    "contents": [
+      {
+        "type": "button",
+        "style": "primary",
+        "action": {
+          "type": "uri",
+          "label": "Primary style button",
+          "uri": "https://example.com"
+        }
+      },
+      {
+        "type": "button",
+        "style": "secondary",
+        "action": {
+          "type": "uri",
+          "label": "Secondary style button",
+          "uri": "https://example.com"
+        }
+      },
+      {
+        "type": "button",
+        "style": "link",
+        "action": {
+          "type": "uri",
+          "label": "Link style button",
+          "uri": "https://example.com"
+        }
+      }
+    ]
+  }
+}
+{
+  "type": "bubble",
+  "body": {
+    "type": "box",
+    "layout": "horizontal",
+    "contents": [
+      {
+        "type": "image",
+        "url": "https://example.com/flex/images/image.jpg",
+        "size": "md"
+      }
+    ]
+  }
+}
+{
+  "type": "bubble",
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "box",
+        "layout": "baseline",
+        "contents": [
+          {
+            "type": "icon",
+            "url": "https://example.com/flex/images/icon.png",
+            "size": "md"
+          },
+          {
+            "type": "text",
+            "text": "The quick brown fox jumps over the lazy dog",
+            "size": "md"
+          }
+        ]
+      },
+      {
+        "type": "box",
+        "layout": "baseline",
+        "contents": [
+          {
+            "type": "icon",
+            "url": "https://example.com/flex/images/icon.png",
+            "size": "lg"
+          },
+          {
+            "type": "text",
+            "text": "The quick brown fox jumps over the lazy dog",
+            "size": "lg"
+          }
+        ]
+      },
+      {
+        "type": "box",
+        "layout": "baseline",
+        "contents": [
+          {
+            "type": "icon",
+            "url": "https://example.com/flex/images/icon.png",
+            "size": "xl"
+          },
+          {
+            "type": "text",
+            "text": "The quick brown fox jumps over the lazy dog",
+            "size": "xl"
+          }
+        ]
+      },
+      {
+        "type": "box",
+        "layout": "baseline",
+        "contents": [
+          {
+            "type": "icon",
+            "url": "https://example.com/flex/images/icon.png",
+            "size": "xxl"
+          },
+          {
+            "type": "text",
+            "text": "The quick brown fox jumps over the lazy dog",
+            "size": "xxl"
+          }
+        ]
+      },
+      {
+        "type": "box",
+        "layout": "baseline",
+        "contents": [
+          {
+            "type": "icon",
+            "url": "https://example.com/flex/images/icon.png",
+            "size": "3xl"
+          },
+          {
+            "type": "text",
+            "text": "The quick brown fox jumps over the lazy dog",
+            "size": "3xl"
+          }
+        ]
+      }
+    ]
+  }
+}
+{
+  "type": "bubble",
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Closing the distance",
+        "size": "md",
+        "align": "center",
+        "color": "#ff0000"
+      },
+      {
+        "type": "text",
+        "text": "Closing the distance",
+        "size": "lg",
+        "align": "center",
+        "color": "#00ff00"
+      },
+      {
+        "type": "text",
+        "text": "Closing the distance",
+        "size": "xl",
+        "align": "center",
+        "weight": "bold",
+        "color": "#0000ff"
+      }
+    ]
+  }
+}
+{
+  "type": "bubble",
+  "body": {
+    "type": "box",
+    "layout": "horizontal",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      }
+    ]
+  }
+}
+{
+  "type": "bubble",
+  "body": {
+    "type": "box",
+    "layout": "horizontal",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod\n tempor incididunt ut labore et dolore magna aliqua.",
+        "wrap": true
+      }
+    ]
+  }
+}
+{
+  "type": "bubble",
+  "body": {
+    "type": "box",
+    "layout": "horizontal",
+    "contents": [
+      {
+        "type": "text",
+        "text": "hello, world",
+        "contents": [
+          {
+            "type": "span",
+            "text": "Hello, world!",
+            "decoration": "line-through"
+          },
+          {
+            "type": "span",
+            "text": "\nClosing",
+            "color": "#ff0000",
+            "size": "sm",
+            "weight": "bold",
+            "decoration": "none"
+          },
+          {
+            "type": "span",
+            "text": " "
+          },
+          {
+            "type": "span",
+            "text": "the",
+            "size": "lg",
+            "color": "#00ff00",
+            "decoration": "underline",
+            "weight": "bold"
+          },
+          {
+            "type": "span",
+            "text": " "
+          },
+          {
+            "type": "span",
+            "text": "distance",
+            "color": "#0000ff",
+            "weight": "bold",
+            "size": "xxl"
+          }
+        ],
+        "wrap": true,
+        "align": "center"
+      }
+    ]
+  }
+}
+{
+  "type": "bubble",
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "spacing": "md",
+    "contents": [
+      {
+        "type": "box",
+        "layout": "horizontal",
+        "spacing": "md",
+        "contents": [
+          {
+            "type": "text",
+            "text": "orange"
+          },
+          {
+            "type": "separator"
+          },
+          {
+            "type": "text",
+            "text": "apple"
+          }
+        ]
+      },
+      {
+        "type": "separator"
+      },
+      {
+        "type": "box",
+        "layout": "horizontal",
+        "spacing": "md",
+        "contents": [
+          {
+            "type": "text",
+            "text": "grape"
+          },
+          {
+            "type": "separator"
+          },
+          {
+            "type": "text",
+            "text": "lemon"
+          }
+        ]
+      }
+    ]
+  }
+}
+{
+  "type": "bubble",
+  "body": {
+    "type": "box",
+    "layout": "horizontal",
+    "contents": [
+      {
+        "type": "image",
+        "url": "https://example.com/flex/images/image.jpg"
+      },
+      {
+        "type": "filler"
+      },
+      {
+        "type": "image",
+        "url": "https://example.com/flex/images/image.jpg"
+      }
+    ]
+  }
+}
