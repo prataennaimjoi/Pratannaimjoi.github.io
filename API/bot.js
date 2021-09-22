77fd1ae3ad204a59cbbba9837df208312525876e
แทน <89076276+prataennaimjoi@users.noreply.github.com>
2021-09-22 11:20:53 +0700

    Update bot.js

--- API/bot.js
+++ API/bot.js
@@ -3,44 +3,44 @@
   "text": "Hello, World!",
   "size": "xl",
   "weight": "bold",
-  "color": "#0000ff"
+  "color": ""
 }
 {
   "type": "text",
   "text": "line://app/1626602314-Vrp0l7Ae?type=text&text=Your%20Text",
   "size": "xl",
   "weight": "bold",
-  "color": "#0000ff"
+  "color": ""
 }
 {
-  "type": "text",
+  "type": "image",
   "image": "line://app/1626602314-Vrp0l7Ae?type=image&img=https://wallpaperstudio10.com/static/wpdb/wallpapers/1000x563/168891.jpg",
   "size": "xl",
   "type : "=> imageimg => Link (must be HTTPS)",
-  "color": "#0000ff"
+  "color": ""
 }
 {
-  "type": "text",
+  "type": "video",
   "video": "line://app/1626602314-Vrp0l7Ae?type=video&ocu=https://tinyurl.com/y8og3or5&piu=https://images6.alphacoders.com/710/thumb-350-710132.png",
   "size": "xl",
   "type : "=> video ocu => video url piu => preview image",
-  "color": "#0000ff"
+  "color": ""
 }
 {
-  "type": "text",
+  "type": "sticker",
   "sticker": "line://app/1626602314-Vrp0l7Ae?type=sticker&stk=anim&sid=32128231&pkg=3099312",
   "size": "xl",
   "type : "=> sticker stk => anim / noanim sid => sticker id pkg => packages id",
   "With Animation: ""
-  "color": "#0000ff"
+  "color": ""
 }
 {
-  "type": "text",
+  "type": "sticker",
   "sticker": "line://app/1626602314-Vrp0l7Ae?type=sticker&stk=noanim&sid=32128231&pkg=3099312",
   "size": "xl",
   "type : "=> sticker stk => anim / noanim sid => sticker id pkg => packages id",
   "No Animation: ""
-  "color": "#0000ff"
+  "color": ""
 }
 
 
