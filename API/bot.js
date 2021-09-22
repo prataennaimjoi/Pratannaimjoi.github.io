c24861ec16518ffb73625faba4148091ff62dbeb
แทน <89076276+prataennaimjoi@users.noreply.github.com>
2021-09-22 11:15:33 +0700

    Update bot.js

--- API/bot.js
+++ API/bot.js
@@ -14,31 +14,31 @@
 }
 {
   "type": "text",
-  "text": "line://app/1626602314-Vrp0l7Ae?type=image&img=https://wallpaperstudio10.com/static/wpdb/wallpapers/1000x563/168891.jpg",
+  "image": "line://app/1626602314-Vrp0l7Ae?type=image&img=https://wallpaperstudio10.com/static/wpdb/wallpapers/1000x563/168891.jpg",
   "size": "xl",
-  "image": "type => imageimg => Link (must be HTTPS)",
+  "type : "=> imageimg => Link (must be HTTPS)",
   "color": "#0000ff"
 }
 {
   "type": "text",
-  "text": "line://app/1626602314-Vrp0l7Ae?type=video&ocu=https://tinyurl.com/y8og3or5&piu=https://images6.alphacoders.com/710/thumb-350-710132.png",
+  "video": "line://app/1626602314-Vrp0l7Ae?type=video&ocu=https://tinyurl.com/y8og3or5&piu=https://images6.alphacoders.com/710/thumb-350-710132.png",
   "size": "xl",
-  "video": "type => video ocu => video url piu => preview image",
+  "type : "=> video ocu => video url piu => preview image",
   "color": "#0000ff"
 }
 {
   "type": "text",
-  "text": "line://app/1626602314-Vrp0l7Ae?type=sticker&stk=anim&sid=32128231&pkg=3099312",
+  "sticker": "line://app/1626602314-Vrp0l7Ae?type=sticker&stk=anim&sid=32128231&pkg=3099312",
   "size": "xl",
-  "sticker": "type => sticker stk => anim / noanim sid => sticker id pkg => packages id",
+  "type : "=> sticker stk => anim / noanim sid => sticker id pkg => packages id",
   "With Animation: ""
   "color": "#0000ff"
 }
 {
   "type": "text",
-  "text": "line://app/1626602314-Vrp0l7Ae?type=sticker&stk=noanim&sid=32128231&pkg=3099312",
+  "sticker": "line://app/1626602314-Vrp0l7Ae?type=sticker&stk=noanim&sid=32128231&pkg=3099312",
   "size": "xl",
-  "sticker": "type => sticker stk => anim / noanim sid => sticker id pkg => packages id",
+  "type : "=> sticker stk => anim / noanim sid => sticker id pkg => packages id",
   "No Animation: ""
   "color": "#0000ff"
 }
