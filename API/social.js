b5c694a0d72df81a16a2e6e025b98d6ebde92f59
แทน <89076276+prataennaimjoi@users.noreply.github.com>
2021-09-23 20:32:01 +0700

    Delete social.js

--- API/social.js
+++
@@ -1,6 +0,0 @@
-<div class="line-it-button" data-lang="th" data-type="share-c" data-ver="3" data-url="https://prataennaimjoi.github.io/Pratannaimjoi.github.io/" data-color="default" data-size="small" data-count="true" style="display: none;"></div>
- <script src="https://www.line-website.com/social-plugins/js/thirdparty/loader.min.js" async="async" defer="defer"></script>
-<div class="line-it-button" data-lang="th" data-type="friend" data-lineid="@642xtzwc" data-count="true" data-home="true" style="display: none;"></div>
- <script src="https://www.line-website.com/social-plugins/js/thirdparty/loader.min.js" async="async" defer="defer"></script>
-<div class="line-it-button" data-lang="th" data-type="like" data-url="https://prataennaimjoi.github.io/Pratannaimjoi.github.io/" data-lineid="@lineteamjp" style="display: none;"></div>
- <script src="https://www.line-website.com/social-plugins/js/thirdparty/loader.min.js" async="async" defer="defer"></script>
